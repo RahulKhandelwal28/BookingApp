bb5414f4e1817ebd458b6f9ec4298a58d6c51594
RahulKhandelwal28 <khandelwalrahul743@gmail.com>
2023-11-18 18:24:36 +0530

    new feature

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
 
 const List = () => {
   return (
     <div>
-      Lists
+      <Navbar/>
+      <Header type="List" />
     </div>
   )
 }
